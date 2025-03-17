83ea8345d4bf66f2582a60d698e00458212c0f63
duicn187532 <duicn187532@gmail.com>
2025-03-17 15:32:31 +0800

    debug

--- main/meetingbooking/src/components/TitleDescription.tsx
+++ main/meetingbooking/src/components/TitleDescription.tsx
@@ -4,7 +4,7 @@ const TitleDescription = () => (
     <div className="mt-4 mx-4 px-2 py-1 bg-white">
       <h1 className="text-xl font-bold">數金部會議室 (11F)</h1>
       <p className="text-xs text-gray-500 my-1">
-        在這裡，您可以選擇合適的會議室。
+        需要會議室？點擊預約，讓會議準備更輕鬆！
       </p>
     </div>
   );
